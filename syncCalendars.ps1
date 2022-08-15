9bccad21bf01b714f01c289f4c88be02d78c773a
billmanh <william.jeffrey.harding@gmail.com>
2022-08-15 10:22:39 -0700

    updated to automatically get the full week

--- syncCalendars.ps1
+++ syncCalendars.ps1
@@ -2,46 +2,55 @@
 $meetingSubject = "Accenture Internal Meeting"
 $clientMeetingSubject = "Client Meeting"
 
-$startdate = "6/19/2022"
-$enddate = "6/25/2022"
+$direction = "in"
+# $direction = "out"
+
+$today = Get-Date
+$BeginDelta = $today.DayOfWeek.value__ - 0
+$closestSunday = $today.AddDays(-$BeginDelta).Date
+
+$endDelta = $today.DayOfWeek.value__ - 6
+$endingSunday = $today.AddDays(-$endDelta).Date
 
 
-
+if ($direction -eq "in"){
 # Get the calendar
-Function Get-OutlookCalendar
-{
-    Add-type -assembly "Microsoft.Office.Interop.Outlook" | out-null
-    $olFolders = "Microsoft.Office.Interop.Outlook.OlDefaultFolders" -as [type]
-    $outlook = new-object -comobject outlook.application
-    $namespace = $outlook.GetNameSpace("MAPI")
-    $folder = $namespace.getDefaultFolder($olFolders::olFolderCalendar)
-    $folder.items |
-    Select-Object -Property Subject, Start, End, Duration, Location
+    Function Get-OutlookCalendar
+    {
+        Add-type -assembly "Microsoft.Office.Interop.Outlook" | out-null
+        $olFolders = "Microsoft.Office.Interop.Outlook.OlDefaultFolders" -as [type]
+        $outlook = new-object -comobject outlook.application
+        $namespace = $outlook.GetNameSpace("MAPI")
+        $folder = $namespace.getDefaultFolder($olFolders::olFolderCalendar)
+        $folder.items |
+        Select-Object -Property Subject, Start, End, Duration, Location
+    }
+    $calendar = Get-OutlookCalendar | where-object { ($_.start -gt [datetime]$closestSunday) -and ($_.start -lt [datetime]$endingSunday)}
+
+    # Swap out the subject line to preserve confidentiality
+    foreach ($j in $calendar){
+        $j.subject = $meetingSubject
+        $j.Location = "Microsoft Teams Meeting"
+    }
+
+    # Export that calendar to CSV.
+    $calendar | Export-Csv -path ".\data\MyCalendar.csv"
 }
-$calendar = Get-OutlookCalendar | where-object { ($_.start -gt [datetime]$startdate) -and ($_.start -lt [datetime]$enddate)}
 
-# Swap out the subject line to preserve confidentiality
-foreach ($j in $calendar){
-    $j.subject = $meetingSubject
-    $j.Location = "Microsoft Teams Meeting"
-}
-
-# Export that calendar to CSV.
-$calendar | Export-Csv -path ".\data\MyCalendar.csv"
+if ($direction -eq "out"){
+    # Later, get that CSV and load it into memory.
+    $newCalendar = Import-CSV -Path ".\data\MyCalendar.csv"
 
 
-# Later, get that CSV and load it into memory.
-$newCalendar = Import-CSV -Path ".\data\MyCalendar.csv"
-
-
-# Then you creat a meeting item for each item in that list
-foreach ($meet in $newCalendar){
-    $ol = New-Object -ComObject Outlook.Application
-    $meeting = $ol.CreateItem('olAppointmentItem')
-    $meeting.Subject = $meet.subject
-    $meeting.Location = $meet.location
-    $meeting.MeetingStatus = [Microsoft.Office.Interop.Outlook.OlMeetingStatus]::olMeeting
-    $meeting.Start = $meet.start
-    $meeting.Duration = $meet.duration
-    $meeting.Send()
+    # Then you creat a meeting item for each item in that list
+    foreach ($meet in $newCalendar){
+        $ol = New-Object -ComObject Outlook.Application
+        $meeting = $ol.CreateItem('olAppointmentItem')
+        $meeting.Subject = $meet.subject
+        $meeting.Location = $meet.location
+        $meeting.MeetingStatus = [Microsoft.Office.Interop.Outlook.OlMeetingStatus]::olMeeting
+        $meeting.Start = $meet.start
+        $meeting.Duration = $meet.duration
+        $meeting.Send()
+    }
 }
